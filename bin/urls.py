29ef9a6179643801146d8a468d892b04ec7a5706
vanshin <kfx721@hotmail.com>
2023-07-26 16:30:36 +0800

    feat: add url prefix

--- bin/urls.py
+++ bin/urls.py
@@ -1,10 +1,10 @@
 
 urls = (
-    ('/pool/list', 'handler.pool.PoolList'),
-    ('/pool/create', 'handler.pool.PoolCreate'),
-    ('/pool/edit', 'handler.pool.PoolEdit'),
+    ('/fish/pool/list', 'handler.pool.PoolList'),
+    ('/fish/pool/create', 'handler.pool.PoolCreate'),
+    ('/fish/pool/edit', 'handler.pool.PoolEdit'),
 
-    ('/menu/list', 'handler.menu.MenuList'),
-    ('/menu/create', 'handler.menu.MenuCreate'),
-    ('/menu/edit', 'handler.menu.MenuEdit'),
+    ('/fish/menu/list', 'handler.menu.MenuList'),
+    ('/fish/menu/create', 'handler.menu.MenuCreate'),
+    ('/fish/menu/edit', 'handler.menu.MenuEdit'),
 )
